b4b06e0d3fb48abd2102b72fe58202973e75abae
zbw <zbw717138561@163.com>
2020-09-15 20:39:07 -0700

    生成简单的敌人，按照指定路线进行移动

---
+++ Infinite-Expansion/Assets/Scripts/feature_enemy/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10;
+    private Transform[] positions;
+    private int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = Waypoints.positions;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (index > positions.Length - 1)
+        {
+            return;
+        }
+        transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * speed);
+        if (Vector3.Distance(positions[index].position, transform.position) < 0.2f)
+        {
+            index++;
+        }
+        if (index > positions.Length - 1)
+        {
+            ReachDestination();
+        }
+    }
+
+    void ReachDestination()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+
+    void OnDestroy()
+    {
+        EnemySpawner.CountEnemyAlive--;    
+    }
+}
