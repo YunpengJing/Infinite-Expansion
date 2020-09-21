2fbdb9de276de9a01507e00d454d7e2f14eaf557
zbw <zbw717138561@163.com>
2020-09-20 21:39:48 -0700

    update enemy: add hp, slider

---
+++ Infinite-Expansion/Assets/Scripts/feature_enemy/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage = 50;
+    public float speed = 20;
+    private Transform target;
+    public GameObject explosionEffectPrefab;
+    public void SetTarget(Transform _target)
+    {
+        this.target = _target;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        transform.LookAt(target.position);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Enemy")
+        {
+            col.GetComponent<Enemy>().TakeDamage(damage);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Infinite-Expansion/Assets/Scripts/feature_enemy/Enemy.cs
+++ Infinite-Expansion/Assets/Scripts/feature_enemy/Enemy.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Scripting.APIUpdating;
 
 public class Enemy : MonoBehaviour
 {
     public float speed = 10;
+    public int hp = 150;
+    private int totalHp;
+    private Slider hpSlider;
     private Transform[] positions;
     private int index = 0;
 
@@ -13,6 +17,8 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         positions = Waypoints.positions;
+        hpSlider = GetComponentInChildren<Slider>();
+        totalHp = hp;
     }
 
     // Update is called once per frame
@@ -28,11 +34,11 @@ public class Enemy : MonoBehaviour
             return;
         }
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //TODO 调用前进动画
         transform.forward = positions[index].position - transform.position;
         if(Time.deltaTime * speed >= Vector3.Distance (transform.position, positions[index].position)){
             index++;
         }
-        //transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * speed);
         else if (Vector3.Distance(positions[index].position, transform.position) < 0.2f)
         {
             index++;
@@ -51,5 +57,24 @@ public class Enemy : MonoBehaviour
     void OnDestroy()
     {
         EnemySpawner.CountEnemyAlive--;    
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (hp <= 0)
+        {
+            return;
+        }
+        hp -= damage;
+        hpSlider.value = (float)hp / totalHp;
+        if (hp <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        //TODO 调用死亡动画
+        Destroy(this.gameObject);
     }
 }
