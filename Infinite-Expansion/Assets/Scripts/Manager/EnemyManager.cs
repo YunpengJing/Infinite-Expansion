3645451348d0b1fdc5a27497166319d260304fef
lonely7yk <893077810@qq.com>
2020-10-17 01:06:30 -0700

    1. fix the bug of laser turret 2. add enemymanager 3. complete the track of turret

---
+++ Infinite-Expansion/Assets/Scripts/Manager/EnemyManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public float damageFromTurret;
+    public float damageFromHero;
+
+    public float hero1AttackBase;
+    public int hero1DeathCnt;
+
+    // 单例
+    private static EnemyManager instance;
+
+    public static EnemyManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+
+        set
+        {
+            instance = value;
+        }
+    }
+
+    public void Awake()
+    {
+        Instance = this;
+        damageFromTurret = 0;
+        damageFromHero = 0;
+
+        hero1AttackBase = 0;
+        hero1DeathCnt = 0;
+    }
+
+    public void AddDamageFromTurret(float damage)
+    {
+        damageFromTurret += damage;
+    }
+
+    public void AddDamageFromHero(float damage)
+    {
+        damageFromHero += damage;
+    }
+
+    public void AddHero1AttackBase(float damage)
+    {
+        hero1AttackBase += damage;
+    }
+
+    public void IncrHero1DeathCnt()
+    {
+        hero1DeathCnt += 1;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
