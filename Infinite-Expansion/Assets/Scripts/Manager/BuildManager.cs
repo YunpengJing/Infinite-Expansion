22170c9f7d57cc33001f567750a1ecdd88cd17ba
YunpengJing <jer_jing@163.com>
2020-10-22 19:48:34 -0700

    changed two button

--- Infinite-Expansion/Assets/Scripts/Manager/BuildManager.cs
+++ Infinite-Expansion/Assets/Scripts/Manager/BuildManager.cs
@@ -60,7 +60,9 @@ public class BuildManager : MonoBehaviour
         missileTurretCnt = 0;
         laserTurretCnt = 0;
 
-        showBuildRange = true;
+        showBuildRange = false;
+        GameObject buildRange = GameObject.Find("Range");
+        buildRange.GetComponent<Image>().enabled = showBuildRange;
 
         selectedTurretIndex = new List<int>();
         selectedTurretIndex.Add(0);
