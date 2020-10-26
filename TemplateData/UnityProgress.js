f83b6a65762cabef21b6d6ab05f9b8d225ed6012
YunpengJing <jer_jing@163.com>
2020-10-26 02:15:02 -0700

    track changed

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -1,24 +1,3 @@
-function UnityProgress(unityInstance, progress) {
-  if (!unityInstance.Module)
-    return;
-  if (!unityInstance.logo) {
-    unityInstance.logo = document.createElement("div");
-    unityInstance.logo.className = "logo " + unityInstance.Module.splashScreenStyle;
-    unityInstance.container.appendChild(unityInstance.logo);
-  }
-  if (!unityInstance.progress) {    
-    unityInstance.progress = document.createElement("div");
-    unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
-    unityInstance.progress.empty = document.createElement("div");
-    unityInstance.progress.empty.className = "empty";
-    unityInstance.progress.appendChild(unityInstance.progress.empty);
-    unityInstance.progress.full = document.createElement("div");
-    unityInstance.progress.full.className = "full";
-    unityInstance.progress.appendChild(unityInstance.progress.full);
-    unityInstance.container.appendChild(unityInstance.progress);
-  }
-  unityInstance.progress.full.style.width = (100 * progress) + "%";
-  unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
-  if (progress == 1)
-    unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1e542a888b53f5e811ab967e08e95c629c18a3baa94d46d42fd859bd4dc13777
+size 1212
